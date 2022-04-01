a5ec2fef6589fc6f361b88ca004e867087b08a00
Deshawn Brown <brownd726@gmail.com>
2022-04-01 16:08:34 -0500

    removed datetime as the clocks time, time is now fully customizable. added alarm and time for alarm.

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,10 +1,39 @@
-import datetime
-import datetime
-class Alarm_Clock:
-    def __init__(self, alarm_time, is_alarm_on):
-        self.current_time = datetime.datetime.now()
-        self.alarm_time = alarm_time
-        self.is_alarm_on = is_alarm_on
+class AlarmClock:
+    def __init__(self):
+        self.current_time = ''
+        self.alarm_time = ''
+        self.is_alarm_on = False
 
-    def tell_time(self):
-        print(self.current_time.strftime("%X"))
+    def set_time(self):
+        user_input = str(input('set the time.'))
+        self.current_time = user_input
+        print(f'the time is now {user_input}.')
+        return
+
+    def set_clock_alarm(self):
+        user_input = input('press y or n to turn on alarm: ')
+        if user_input == 'y':
+            print('the alarm is on now!')
+            self.is_alarm_on = True
+        elif user_input == 'n':
+            print('the alarm is off.')
+            self.is_alarm_on = False
+        else:
+            print('invalid input')
+        return
+
+    def set_alarm_time(self):
+        if self.is_alarm_on == True:
+            user_input = str(input('set the alarm time.'))
+            self.alarm_time = user_input
+            print(f'the alarm time is now {user_input}.')
+        else:
+            print('alarm not set')
+        return
+
+    def clock_result(self):
+        if self.is_alarm_on == True:
+            print(f'the time is {self.current_time} you have set the alarm to {self.alarm_time}')
+        else:
+            print(f'you have set the time to {self.current_time}, the alarm is off')
+
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import datetime
-from alarm_clock import Alarm_Clock
+from alarm_clock import AlarmClock
+
+clock = AlarmClock()
+clock.set_time()
+clock.set_clock_alarm()
+clock.set_alarm_time()
+clock.clock_result()
 
 
-alarm_time = datetime.time(12, 12, 12)
-is_alarm_on = True
-clock_time = Alarm_Clock(alarm_time,is_alarm_on)
-clock_time.tell_time()
-print(alarm_time)
-
-
-
-# x = datetime.datetime.now()
-
-# print(x.strftime("%X"))
